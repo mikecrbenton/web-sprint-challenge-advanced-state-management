65461db58a8b19dd08efbe4af46d6e40bab3e4d9
mikecrbenton <mikecrbenton@hotmail.com>
2020-08-29 12:34:48 -0500

    stole form from usercard project and modified, post is working

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -14,9 +14,26 @@ export const initializeSmurf = () => {
 }
 
 export const getSmurf = () => {
-
+   return dispatch => {
+      console.log("IN DISPACTH FOR GETSMURF")
+       
+       axios.get("http://localhost:3333/smurfs")
+         .then( (res) => {
+            console.log("SMURFS IN action ",res);
+            dispatch({ type: "GET_SMURF", payload: res.data });
+         })
+         .catch(err => console.log("ERROR:", err));
+     };
 }
 
-export const postSmurf = () => {
+export const postSmurf = (newSmurf) => {
+   return dispatch => {
 
+      axios.post( "http://localhost:3333/smurfs", newSmurf )
+      .then( (response) => {
+         console.log("RESPONSE FROM API: ",response.data)
+         dispatch({ type: "POST_SMURF", payload: response.data })
+         dispatch({ type: "GET_SMURF", payload: response.data });
+       } )
+   } 
 }
--- smurfs/src/components/smurf_form.js
+++ smurfs/src/components/smurf_form.js
@@ -1,10 +1,84 @@
-import React from 'react'
+import React, { useState } from "react";
+import { connect } from 'react-redux';
+import { getSmurf, postSmurf } from '../actions'
+import styled from 'styled-components';
+import axios from 'axios';
 
 function SmurfForm(props) {
 
-   return (
-      <p>working</p>
-   )
+  const [newSmurf, setNewSmurf] = useState(
+     {
+        name: "",
+        age: "",
+        height: ""
+     }
+  )
+ 
+  const changeHandler = e => {
+     e.persist();
+
+     setNewSmurf( {
+       ...newSmurf,
+       [e.target.name] : e.target.value
+     });
+  };
+
+  
+  const submitHandler = e => {
+    e.preventDefault();
+    console.log("post is called in submit")
+    props.postSmurf(newSmurf)
+  
+    //clear form
+    setNewSmurf({
+      name: "",
+      age: "",
+      height: ""
+   });
+  };
+   
+    return (
+       <>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="name">
+           Name
+            <input
+               type="text"
+               name="name"
+               onChange={changeHandler} // setState
+               value={newSmurf.name} //controlled input - view State
+            />
+        </label>
+        <label htmlFor="age">
+           Age 
+            <input
+               type="text"
+               name="age"
+               onChange={changeHandler} // setState
+               value={newSmurf.age} //controlled input - view State
+            />
+        </label>
+        <label htmlFor="height">
+           Height
+            <input
+               type="text"
+               name="height"
+               onChange={changeHandler} // setState
+               value={newSmurf.height} //controlled input-view State
+            />
+        </label>
+        <button type="submit">Add</button>
+        <button type="button" onClick={props.getSmurf}>Get Smurfs</button>
+      </form>
+     
+       </>
+    );
 }
 
-export default SmurfForm;
+const mapStateToProps = (state) => {
+   return{
+     
+   }
+}
+
+export default connect(mapStateToProps, { postSmurf, getSmurf })(SmurfForm);
